05a4beb587855b97b773ce26dfbd9c9503911fa1
Mai Nguyen Thi <maint.194@gmail.com>
2021-04-15 18:04:57 +0700

    Added component 1

---
+++ projects/ng-main/src/modules/nors-component-1/nors-component-1.component.ts
@@ -0,0 +1,10 @@
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'div[account-login]',
+  templateUrl: 'login.component.html'
+})
+
+export class NorsComponent1Component {
+
+}
